82b73e54f9c68549ab7b9586cad6d1e377198628
best2025j <audugodtime95@gmai.com>
2023-10-05 09:26:27 +0100

    border radius fixed

--- src/components/ui/Corousel.jsx
+++ src/components/ui/Corousel.jsx
@@ -13,12 +13,14 @@ const Carousel = () => {
   return (
     <section>
       <div className="h-full w-[85%] mx-auto sm:mx-0 sm:w-full">
-        <div className="rounded-full bg-red-500 h-[90vh] w-[90%]">1</div>
+        <div className="rounded-full bg-red-500 h-[109px] w-[1455px] flex-shrink-0">
+          1
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
           {staffData.map((image, index) => (
             <div
               key={index}
-              className="justify-center items-center hover:bg-slate-100 rounded-3xl p-2 flex hover:scale-105 ease-in duration-300 dark:bg-slate-800 flex-col"
+              className="justify-center items-center hover:bg-slate-100 rounded-3xl p-2 flex  dark:bg-slate-800 flex-col"
             >
               <img
                 className="h-48 w-96 md:h-full md:w-full"
@@ -28,7 +30,9 @@ const Carousel = () => {
             </div>
           ))}
         </div>
-        <div className="rounded-full bg-red-500 h-[90vh] w-[90%]">1</div>
+        <div className="rounded-full bg-red-500 h-[109px] w-[1455px] flex-shrink-0">
+          1
+        </div>
       </div>
       <h1>welcome gt</h1>
     </section>
