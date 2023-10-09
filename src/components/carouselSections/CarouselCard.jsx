26fe987ad697cba04eefcdade44679881fb1a6b9
best2025j <audugodtime95@gmai.com>
2023-10-09 10:22:15 +0100

    feat: card added

--- src/components/carouselSections/CarouselCard.jsx
+++ src/components/carouselSections/CarouselCard.jsx
@@ -1,14 +1,47 @@
 import React from "react";
 
 const CarouselCard = () => {
+  const staffData = [
+    {
+      image: "/Courses card for html.png",
+    },
+    {
+      image: "/Courses card for html (1).png",
+    },
+    {
+      image: "/Courses card for html.png",
+    },
+    {
+      image: "/Courses card for html (1).png",
+    },
+    // {
+    //   image: "/KTA.png",
+    // },
+  ];
+
   return (
-    <div>
-      <div>
-        <h1>01. Introduction to HTML and CSS</h1>
+    <div className="ml-32 py-32 space-y-[110px]">
+      <div className="h-full w-full space-y-3">
+        <h1 className="sm:text-5xl text-xl font-bold">Explore our courses</h1>
+        <div className="max-w-[480px]">
+          <p className="text-[#4F616D] text-base">
+            At KTA, we offer a diverse range of tech courses that cater to
+            beginners and experts alike. From web development to data science,
+            our curriculum is designed to equip you with practical skills that
+            matter in today's tech-driven world. Join us on this educational
+            journey and unlock your potential.
+          </p>
+        </div>
       </div>
 
-      <div>
-        <h1>02. Python and Java</h1>
+      <div className="grid grid-col-1 md:grid-cols-2 gap-6 py-6">
+        {staffData.map((item, index) => (
+          <div className="" key={index}>
+            <div className="flex">
+              <img src={item.image} alt="" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/ui/OutSponsors.jsx
+++ src/components/ui/OutSponsors.jsx
@@ -28,18 +28,7 @@ const OutSponsors = () => {
         </div>
       </div>
 
-      <div className="h-full w-full py-10 px-6 space-y-3">
-        <h1 className="sm:text-5xl text-xl font-bold">Explore our courses</h1>
-        <div className="max-w-[480px]">
-          <p className="text-[#4F616D] text-base">
-            At KTA, we offer a diverse range of tech courses that cater to
-            beginners and experts alike. From web development to data science,
-            our curriculum is designed to equip you with practical skills that
-            matter in today's tech-driven world. Join us on this educational
-            journey and unlock your potential.
-          </p>
-        </div>
-      </div>
+   
     </section>
   );
 };
