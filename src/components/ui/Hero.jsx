2b98eeb384d98788cf99a5bd8c0ffac2aa0c9a65
best2025j <audugodtime95@gmai.com>
2023-10-11 10:44:52 +0100

    animations added

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="md:max-w-[852px]  space-y-2">
         <h1 className="bg-[#4F616D] rounded-full text-white p-2 w-44 text-center mx-auto ">Kwaratech Academy</h1>
         <h1 className="font-bold md:text-6xl text-xl">
-          Your Journey to a <span className="text-[#00AFF0]">rewarding</span>
+          Your Journey to a <span className="text-[#00AFF0] bg-white">rewarding</span>
            <br />tech career begins now !
         </h1>
       </div>
